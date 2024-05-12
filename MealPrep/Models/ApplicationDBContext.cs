0b3d10ca57cc539f0dbe268ab4238fe8df597881
Lilian <Lilian@DESKTOP-7FL6DRO>
2024-05-12 15:06:26 -0700

    added dbcontext & migrations for personal computor & updated program.cs

---
+++ MealPrep/Models/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealPrep.Models
+{
+	
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        //protected override void 
+        //public DbSet<Project> Projects { get; set; }
+        //public DbSet<Client> Clients { get; set; }
+
+        
+    }
+}
+
--- MealPrep/Program.cs
+++ MealPrep/Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using MealPrep.Models;
+using Microsoft.AspNetCore.Identity;
+using MealPrep.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+//var connectionString = builder.Configuration.GetConnectionString("LoginDBContextConnection") ?? throw new InvalidOperationException("Connection string 'LoginDBContextConnection' not found.");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlite("Data Source = prello.db"));
+
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<LoginDBContext>();
+//builder.Services.AddDbContext<LoginDBContext>(options => options.UseSqlite("Data Source=Users.db"));
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -18,10 +30,11 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
