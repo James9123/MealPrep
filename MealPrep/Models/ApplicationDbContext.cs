36288d801c7e9bf55161814d99a836a8b9bbf8f9
Lilian <Lilian@DESKTOP-7FL6DRO>
2024-06-02 15:27:47 -0700

    Edited css and will work on adding meals to db

--- MealPrep/Models/ApplicationDbContext.cs
+++ MealPrep/Models/ApplicationDbContext.cs
@@ -19,7 +19,32 @@ namespace MealPrep.Models
 
         public DbSet<MealPlan> MyMeals { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<Meal>().HasData(new Meal
+            {
+                Id = 1,
+                Title = "Roast Steak",
+                Description = "A very fancy steak",
+                ImageUrl = "./images/9.jpg",
+            });
+            modelBuilder.Entity<Meal>().HasData(new Meal
+            {
+                Id = 2,
+                Title = "Ice Cream",
+                Description = "A delectable dessert",
+                ImageUrl = "./images/7.jpg",
+            });
+            modelBuilder.Entity<Meal>().HasData(new Meal
+            {
+                Id = 3,
+                Title = "Burrito",
+                Description = "Has Everything",
+                ImageUrl = "./images/3.jpg",
+            });
+        }
 
 
         //public DbSet<Meal> MyMeals { get; set; }
